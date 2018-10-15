4899d7161456aa94307abe869abac8432680830a
zm17443 <zm17443@josai.ac.jp>
2018-10-14 21:59:33 -0400

    date

---
+++ ex06-5.js
@@ -0,0 +1,11 @@
+var who65 = 'world';
+function sayhello65(){
+  alert('Hello,' + who65 +'!');
+}
+function ikeda65(){
+  who65 = '池田';
+ 
+}
+function aya65(){
+  who65 = '彩';
+ }
---
+++ ex06-7.js
@@ -0,0 +1,8 @@
+var who67 ='world';
+function sayhello67(){
+  alert('Hello,'+ who67 +'!');
+}
+function someone67(X){
+  who67 = X;
+ 
+}
