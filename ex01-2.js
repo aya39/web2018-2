95f901111c1914d0dba2ddfed51a33a977a85489
zm17443 <zm17443@josai.ac.jp>
2019-01-06 20:48:27 -0500

    date

---
+++ ex01-2.js
@@ -0,0 +1,16 @@
+function hyoji(){
+  //表示する文字
+ var str="ながのかわあや";
+ //
+ var cnt=document.timer.moji.value.length;
+  //
+  if(cnt<7){
+    //
+    document.timer.moji.value=str.substr(0,cnt+1);}
+  else{
+    //
+    document.timer.moji.value="";}}
+function startfnc(){
+  //
+  setInterval("hyoji()",1000);}
+
---
+++ ex01-3.js
@@ -0,0 +1,3 @@
+function imgchange2(){
+  document.getElementById('logo2').src='./aya1.JPG';
+}
