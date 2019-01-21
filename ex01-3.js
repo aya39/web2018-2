d14e3e6ddb481f40c18c6bc103f9f85dd6fcbbb4
zm17443 <zm17443@josai.ac.jp>
2019-01-20 20:52:19 -0500

    date

--- ex01-3.js
+++ ex01-3.js
@@ -1,3 +1,12 @@
+function txtchange2(){
+  document.getElementById('txt2').innerHTML='モンブラン！！';
+}
+function txtchange3(){
+  document.getElementById('txt3').innerHTML='HAWAII！！';
+}
+ 
+
+
 function imgchange2(){
-  document.getElementById('logo2').src='./aya1.JPG';
+  document.getElementById('logo2').src='./aya.jpg';
 }
